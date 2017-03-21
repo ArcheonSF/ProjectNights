1659516ed5d9f52a64d250bc8c2ca60f365f09d6
ArcheonSF <simon.friling@gmail.com>
2017-03-21 21:44:37 +0000

    21/3/2017

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof (ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float walkMoveStopRadius = 0.2f;
+
+    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+        
+    private void Start()
+    {
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+
+        //TODO: FIX ISSUE WITH CLICK TO MOVE AND WASD
+        //TODO: ALSO FIX ISSUE WITH WALKING ANIMATION
+        if (Input.GetMouseButton(0))
+        {
+            print("Cursor raycast hit layer " + cameraRaycaster.layerHit);
+            switch (cameraRaycaster.layerHit)
+            {
+                case Layer.Walkable:
+                    currentClickTarget = cameraRaycaster.hit.point;                    
+                    break;
+                case Layer.Enemy:
+                    print("Not Moving To Enemy");
+                    break;
+                default:
+                    print("Unexpected layer found");
+                    break;
+            }
+            
+        }
+
+        //stop spinning in circles due to overshooting!
+        var playerToClickPoint = currentClickTarget - transform.position;
+        if (playerToClickPoint.magnitude >= walkMoveStopRadius)
+        {
+            m_Character.Move(playerToClickPoint, false, false);
+        }
+        else
+        {
+            m_Character.Move(Vector3.zero, false, false);
+        }
+
+        
+
+    }
+}
+
